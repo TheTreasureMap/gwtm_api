15c8bac9fe631e52c88c4362c659597d390c719d
Samuel Wyatt <swyatt@email.arizona.edu>
2023-12-04 16:44:24 -0700

    adding coverage calc and skymap renormalization

--- src/gwtm_api/event_tools.py
+++ src/gwtm_api/event_tools.py
@@ -1,6 +1,7 @@
 from matplotlib import pyplot as plt
 from matplotlib.patches import Circle, Wedge, Polygon
 import numpy as np
+import astropy
 from astropy.coordinates import SkyCoord
 
 import healpy as hp
@@ -148,3 +149,177 @@ def plot_coverage(api_token: str = None, graceid: str = None, pointings: list = 
                 bbox_transform=fig.transFigure,  ncol=4)
     plt.title(f"Reported Coverage for {graceid}")
     plt.show()
+
+
+def calculate_coverage(api_token: str = None, graceid: str = None, pointings: list = [],
+    cache=False, approximate=True):
+
+    if len(pointings) == 0 and graceid is None:
+        raise Exception("Pointings list or graceid is required")
+
+    if len(pointings) == 0 and graceid is not None:
+        pointings = Pointing.get(graceid = graceid, api_token=api_token, status='completed')
+
+    skymap = Alert.fetch_skymap(graceid=graceid, api_token=api_token, cache=cache)
+
+    alert_info = Alert.get(graceid=graceid, api_token=api_token)
+
+    instrument_ids = list(set([x.instrumentid for x in pointings]))
+    inst_footprints =  Instrument.get(ids=instrument_ids, include_footprint=True, api_token=api_token)
+
+    instrument_enumeration_dict = {}
+    for i, iid in enumerate(instrument_ids):
+        instrument_enumeration_dict[iid] = i
+
+    time_of_signal = alert_info.time_of_signal
+    skymap_nside = hp.npix2nside(len(skymap))
+
+    areas = []
+    times = []
+    probs = []
+    qps = []
+    qpsarea=[]
+    NSIDE4area = 512 #this gives pixarea of 0.013 deg^2 per pixel
+    pixarea = hp.nside2pixarea(NSIDE4area, degrees=True)
+
+    for i in instrument_ids:
+        polygon_arr = []
+        instrument_pointings = [x for x in pointings if x.instrumentid == i]
+
+        if cache:
+            cached_footprints = Footprint.get_cached_footprints(
+                graceid=graceid,
+                instrument_id=i,
+                pointings=instrument_pointings
+            )
+            if cached_footprints:
+                polygon_arr.extend(
+                    cached_footprints
+                )
+
+        inst_footprint = [x for x in inst_footprints if x.id == i][0]
+        
+        if len(polygon_arr) == 0:
+            for p in instrument_pointings:
+                projected_footprint = inst_footprint.project(p.ra, p.dec, p.pos_angle)
+                for ccd in projected_footprint:
+                    ra_deg, dec_deg = zip(*[(coord_deg[0], coord_deg[1])
+                                            for coord_deg in ccd])
+                    
+                    list_o_coords = []
+                    for x, y in zip(ra_deg, dec_deg):
+                        list_o_coords.append((x, y))
+                    polygon_arr.append(list_o_coords)
+
+        if cache:
+            Footprint.put_cached_footprints(
+                polygon_arr,
+                graceid=graceid,
+                instrument_id=i,
+                pointings=instrument_pointings
+            )
+
+        for j,arr in enumerate(polygon_arr):
+            ras_poly = [x[0] for x in arr][:-1]
+            decs_poly = [x[1] for x in arr][:-1]
+            xyzpoly = astropy.coordinates.spherical_to_cartesian(1, np.deg2rad(decs_poly), np.deg2rad(ras_poly))
+            qp = hp.query_polygon(skymap_nside,np.array(xyzpoly).T)
+            qps.extend(qp)
+
+            # qparea = hp.query_polygon(NSIDE4area, np.array(xyzpoly).T)
+            # qpsarea.extend(qparea)
+
+            #deduplicate indices, so that pixels already covered are not double counted
+            deduped_indices=list(dict.fromkeys(qps))
+            # deduped_indices_area = list(dict.fromkeys(qpsarea))
+
+            # area = pixarea * len(deduped_indices_area)
+
+            #prob = 0
+            #for ind in deduped_indices:
+            #    prob += skymap[ind]
+            #elapsed = p.time - time_of_signal
+            #elapsed = elapsed.total_seconds()/3600
+            #times.append(elapsed)
+            #probs.append(prob)
+            
+    prob = np.sum(skymap[deduped_indices])
+    area = pixarea * len(deduped_indices)
+    return prob, area
+
+def renormalize_skymap(api_token: str = None, graceid: str = None, pointings: list = [],
+    cache=False):
+
+
+    if len(pointings) == 0 and graceid is None:
+        raise Exception("Pointings list or graceid is required")
+
+    if len(pointings) == 0 and graceid is not None:
+        pointings = Pointing.get(graceid = graceid, api_token=api_token, status='completed')
+
+    skymap = Alert.fetch_skymap(graceid=graceid, api_token=api_token, cache=cache)
+
+    alert_info = Alert.get(graceid=graceid, api_token=api_token)
+
+    instrument_ids = list(set([x.instrumentid for x in pointings]))
+    inst_footprints =  Instrument.get(ids=instrument_ids, include_footprint=True, api_token=api_token)
+
+    instrument_enumeration_dict = {}
+    for i, iid in enumerate(instrument_ids):
+        instrument_enumeration_dict[iid] = i
+
+    time_of_signal = alert_info.time_of_signal
+    skymap_nside = hp.npix2nside(len(skymap))
+
+    qps = []
+
+    for i in instrument_ids:
+        polygon_arr = []
+        instrument_pointings = [x for x in pointings if x.instrumentid == i]
+
+        if cache:
+            cached_footprints = Footprint.get_cached_footprints(
+                graceid=graceid,
+                instrument_id=i,
+                pointings=instrument_pointings
+            )
+            if cached_footprints:
+                polygon_arr.extend(
+                    cached_footprints
+                )
+
+        inst_footprint = [x for x in inst_footprints if x.id == i][0]
+        
+        if len(polygon_arr) == 0:
+            for p in instrument_pointings:
+                projected_footprint = inst_footprint.project(p.ra, p.dec, p.pos_angle)
+                for ccd in projected_footprint:
+                    ra_deg, dec_deg = zip(*[(coord_deg[0], coord_deg[1])
+                                            for coord_deg in ccd])
+                    
+                    list_o_coords = []
+                    for x, y in zip(ra_deg, dec_deg):
+                        list_o_coords.append((x, y))
+                    polygon_arr.append(list_o_coords)
+
+        if cache:
+            Footprint.put_cached_footprints(
+                polygon_arr,
+                graceid=graceid,
+                instrument_id=i,
+                pointings=instrument_pointings
+            )
+
+        for j,arr in enumerate(polygon_arr):
+            ras_poly = [x[0] for x in arr][:-1]
+            decs_poly = [x[1] for x in arr][:-1]
+            xyzpoly = astropy.coordinates.spherical_to_cartesian(1, np.deg2rad(decs_poly), np.deg2rad(ras_poly))
+            qp = hp.query_polygon(skymap_nside,np.array(xyzpoly).T)
+            qps.extend(qp)
+
+            deduped_indices=list(dict.fromkeys(qps))
+
+    skymap[deduped_indices] = 0
+    normed_skymap = skymap/np.sum(skymap)
+
+    return normed_skymap
