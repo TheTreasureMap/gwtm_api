69137d3cce01e3ebaf6845515ea5ec9a4a71e008
swyatt7 <swyatt@email.arizona.edu>
2024-05-09 10:45:32 -0700

    adding candidate put/delete/batch_delete funcitons

--- tests/candidate_test.py
+++ tests/candidate_test.py
@@ -55,7 +55,30 @@ def test_batch_post():
     ]
     gwtm_api.Candidate.batch_post(candidates=batch, api_token=API_TOKEN, graceid="MS181101ab")
 
+def put_test():
+    candidate = gwtm_api.Candidate.get(api_token=API_TOKEN, id = 20)
+    my_candidate = candidate[0]
+    my_candidate.candidate_name = "test_update_candidate_name"
+    my_candidate.put(api_token=API_TOKEN, base="http://127.0.0.1:5000/api/")
+    updated = gwtm_api.Candidate.get(api_token=API_TOKEN, id = 20)
+    print("")
+    updated[0].dump()
+
+    candidate = gwtm_api.Candidate(id=20, api_token=API_TOKEN)
+    candidate.put(api_token=API_TOKEN, payload={"tns_name": "update_tns_name"}, base="http://127.0.0.1:5000/api/")
+    candidate.dump()
+
+def del_test():
+    candidate = gwtm_api.Candidate(id=21, api_token=API_TOKEN)
+    candidate.delete(api_token=API_TOKEN, base="http://127.0.0.1:5000/api/")
+
+def batch_delete():
+    gwtm_api.Candidate.batch_delete(api_token=API_TOKEN, ids=[15,16,17], verbose=True)
+
 if __name__ == "__main__":
-    test_post()
-    test_batch_post()
-    test_get()
+    #test_post()
+    #test_batch_post()
+    #test_get()
+    put_test()
+    #del_test()
+    #batch_delete()
