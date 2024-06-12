c1439f0c7358f8a9bd2a6f28e343cb5186927d5a
swyatt7 <swyatt@email.arizona.edu>
2024-06-12 12:11:52 -0700

    easy import

--- src/gwtm_api/__init__.py
+++ src/gwtm_api/__init__.py
@@ -3,4 +3,4 @@ from .candidate import Candidate as Candidate  #noqa: E402
 from .instrument import Instrument as Instrument  # noqa: E402
 from .instrument import Footprint as Footprint  # noqa: E402
 from .alert import Alert as Alert  # noqa: E402
-from .event_tools import plot_coverage, calculate_coverage, renormalize_skymap  # noqa: E402, F401
+from .event_tools import plot_coverage, calculate_coverage, renormalize_skymap, candidate_coverage  # noqa: E402, F401
